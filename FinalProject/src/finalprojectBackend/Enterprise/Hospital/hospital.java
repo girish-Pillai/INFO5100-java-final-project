d09bac6d9799d7ec89dacffae5604cd3f8086905
girish-Pillai <pillai.g@northeastern.edu>
2022-04-27 16:57:17 -0400

    Added Exterprise-->Hospital-->(Doc, Patient)

--- FinalProject/src/finalprojectBackend/Enterprise/Hospital/hospital.java
+++ FinalProject/src/finalprojectBackend/Enterprise/Hospital/hospital.java
@@ -6,14 +6,62 @@ package finalprojectBackend.Enterprise.Hospital;
 
 import finalprojectBackend.Components.Address;
 import finalprojectBackend.Organization.User;
+import finalprojectBackend.Enterprise.Enterprise;
 import java.util.ArrayList;
 
 /**
  *
  * @author girish
  */
-public class hospital extends User{
-    
-    
-    
+public class Hospital extends Enterprise{
+
+    private ArrayList<Doctor> doctordirectory;
+    private ArrayList<Nurse> nursedirectory;
+    private ArrayList<Patient> patientdirectory;
+
+    public Hospital(String enterpriseName, String registeryNumber, String address, String username, String password) {
+        super(enterpriseName, registeryNumber, address, username, password);
+
+        this.doctordirectory = new ArrayList();
+        this.nursedirectory = new ArrayList();
+        this.patientdirectory = new ArrayList();
+
+    }
+
+    public ArrayList<Doctor> getDoctordirectory() {
+        return doctordirectory;
+    }
+
+    public void setDoctordirectory(ArrayList<Doctor> doctordirectory) {
+        this.doctordirectory = doctordirectory;
+    }
+
+    public ArrayList<Nurse> getNursedirectory() {
+        return nursedirectory;
+    }
+
+    public void setNursedirectory(ArrayList<Nurse> nursedirectory) {
+        this.nursedirectory = nursedirectory;
+    }
+
+    public ArrayList<Patient> getPatientdirectory() {
+        return patientdirectory;
+    }
+
+    public void setPatientdirectory(ArrayList<Patient> patientdirectory) {
+        this.patientdirectory = patientdirectory;
+    }
+
+    public void addDoctor(Doctor doc) {
+        doctordirectory.add(doc);
+    }
+
+    public void addNurse(Nurse nur) {
+        nursedirectory.add(nur);
+    }
+
+    public void addPatient(Patient pat) {
+        patientdirectory.add(pat);
+    }
 }
+
