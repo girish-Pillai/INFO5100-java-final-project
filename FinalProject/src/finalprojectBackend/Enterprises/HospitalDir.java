0f4635a8c8dada7c32c3892c7929585cad9212c6
girish-Pillai <pillai.g@northeastern.edu>
2022-04-26 22:59:59 -0400

    Added new Ui and Landing page

---
+++ FinalProject/src/finalprojectBackend/Enterprises/HospitalDir.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package finalprojectBackend.Enterprises;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author girish
+ */
+public class HospitalDir {
+    
+    private ArrayList<Hospital> listOfHospitals;
+
+    
+
+    public HospitalDirectory() {
+        listOfHospitals = new ArrayList();
+    }
+
+    public void addHospital(Hospital hospital) {
+        listOfHospitals.add(hospital);
+    }
+
+    public void removeHospital(String id) {
+
+        for (Hospital hosp : listOfHospitals) {
+            if (hosp.getRegisteryNumber() == id) {
+                listOfHospitals.remove(hosp);
+            }
+        }
+
+    }
+    
+    public ArrayList<Hospital> getListOfHospitals() {
+        return listOfHospitals;
+    }
+
+    public void setListOfHospitals(ArrayList<Hospital> listOfHospitals) {
+        this.listOfHospitals = listOfHospitals;
+    }
+
+    public Hospital findHospital(String hospitalName) {
+        System.out.println("findHospital() "+hospitalName);
+        for (Hospital hosp : listOfHospitals) {
+            if (hosp.getEnterpriseName() == hospitalName) {
+                return hosp;
+            }
+        }
+        return null;
+    }
+    
+}
