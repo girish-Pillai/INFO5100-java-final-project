b0c560aca5af67cab62d213c236033436480b4fa
pathakprasad <pathak.pra@northeastern.edu>
2022-04-28 16:57:00 -0400

    Fixed Imports for Operating System Java Class

--- FinalProject/src/finalprojectBackend/OperatingSystem/OperatingSystem.java
+++ FinalProject/src/finalprojectBackend/OperatingSystem/OperatingSystem.java
@@ -4,6 +4,20 @@
  */
 package finalprojectBackend.OperatingSystem;
 
+import finalprojectBackend.Directories.EnterpriseDirectories.HospitalDirectory;
+import finalprojectBackend.Enterprise.DonorBank.DonorUser;
+import finalprojectBackend.Enterprise.DonorBank.DonorBank;
+import finalprojectBackend.Enterprise.Hospital.Doc;
+import finalprojectBackend.Enterprise.Hospital.Hospital;
+import finalprojectBackend.Enterprise.Hospital.Nurse;
+import finalprojectBackend.Enterprise.Hospital.Patient;
+import finalprojectBackend.Enterprise.Lab.Technician;
+import finalprojectBackend.Enterprise.Logistics.Handler;
+import finalprojectBackend.Enterprise.Logistics.Vehicle;
+import finalprojectBackend.Organization.DonateEntity;
+import finalprojectBackend.Organization.User;
+import java.util.ArrayList;
+
 /**
  *
  * @author prasa
