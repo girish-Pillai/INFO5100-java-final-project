c5ec30335c8744089674bc5b411aad68c9025eb7
pathakprasad <pathak.pra@northeastern.edu>
2022-04-28 18:07:38 -0400

    Added variables and getter setter methods for all memebers of the Operating System Java Class

--- FinalProject/src/finalprojectBackend/OperatingSystem/OperatingSystem.java
+++ FinalProject/src/finalprojectBackend/OperatingSystem/OperatingSystem.java
@@ -14,7 +14,7 @@ import finalprojectBackend.Enterprise.Hospital.Patient;
 import finalprojectBackend.Enterprise.Lab.Technician;
 import finalprojectBackend.Enterprise.Logistics.Handler;
 import finalprojectBackend.Enterprise.Logistics.Vehicle;
-import finalprojectBackend.Organization.DonateEntity;
+import finalprojectBackend.Organization.DonationAssignment;
 import finalprojectBackend.Organization.User;
 import java.util.ArrayList;
 
@@ -24,5 +24,103 @@ import java.util.ArrayList;
  * This is class for all functional methods of the Application
  */
 public class OperatingSystem {
-    
+    private static OperatingSystem os;
+    private HospitalDirectory hospitalDirectory;
+    private ArrayList<Doc> doctorDirectory;
+    private ArrayList<DonorUser> donorUserDirectory;
+    private ArrayList<DonorBank> donorbankDirectory;
+    private ArrayList<Nurse> nurseDirectory;
+    private ArrayList<Patient> patientdirectory;
+    private ArrayList<DonationAssignment> donationAssignmentList;
+    private ArrayList<Handler> handlerDirectory;
+    private ArrayList<Vehicle> vehicleDirectory;
+    private ArrayList<Technician> technicianDirectory;
+
+    public static OperatingSystem getOs() {
+        return os;
+    }
+
+    public static void setOs(OperatingSystem os) {
+        OperatingSystem.os = os;
+    }
+
+    public HospitalDirectory getHospitalDirectory() {
+        return hospitalDirectory;
+    }
+
+    public void setHospitalDirectory(HospitalDirectory hospitalDirectory) {
+        this.hospitalDirectory = hospitalDirectory;
+    }
+
+    public ArrayList<Doc> getDoctorDirectory() {
+        return doctorDirectory;
+    }
+
+    public void setDoctorDirectory(ArrayList<Doc> doctorDirectory) {
+        this.doctorDirectory = doctorDirectory;
+    }
+
+    public ArrayList<DonorUser> getDonorUserDirectory() {
+        return donorUserDirectory;
+    }
+
+    public void setDonorUserDirectory(ArrayList<DonorUser> donorUserDirectory) {
+        this.donorUserDirectory = donorUserDirectory;
+    }
+
+    public ArrayList<DonorBank> getDonorbankDirectory() {
+        return donorbankDirectory;
+    }
+
+    public void setDonorbankDirectory(ArrayList<DonorBank> donorbankDirectory) {
+        this.donorbankDirectory = donorbankDirectory;
+    }
+
+    public ArrayList<Nurse> getNurseDirectory() {
+        return nurseDirectory;
+    }
+
+    public void setNurseDirectory(ArrayList<Nurse> nurseDirectory) {
+        this.nurseDirectory = nurseDirectory;
+    }
+
+    public ArrayList<Patient> getPatientdirectory() {
+        return patientdirectory;
+    }
+
+    public void setPatientdirectory(ArrayList<Patient> patientdirectory) {
+        this.patientdirectory = patientdirectory;
+    }
+
+    public ArrayList<DonationAssignment> getDonationAssignmentList() {
+        return donationAssignmentList;
+    }
+
+    public void setDonationAssignmentList(ArrayList<DonationAssignment> donationAssignmentList) {
+        this.donationAssignmentList = donationAssignmentList;
+    }
+
+    public ArrayList<Handler> getHandlerDirectory() {
+        return handlerDirectory;
+    }
+
+    public void setHandlerDirectory(ArrayList<Handler> handlerDirectory) {
+        this.handlerDirectory = handlerDirectory;
+    }
+
+    public ArrayList<Vehicle> getVehicleDirectory() {
+        return vehicleDirectory;
+    }
+
+    public void setVehicleDirectory(ArrayList<Vehicle> vehicleDirectory) {
+        this.vehicleDirectory = vehicleDirectory;
+    }
+
+    public ArrayList<Technician> getTechnicianDirectory() {
+        return technicianDirectory;
+    }
+
+    public void setTechnicianDirectory(ArrayList<Technician> technicianDirectory) {
+        this.technicianDirectory = technicianDirectory;
+    }
 }
